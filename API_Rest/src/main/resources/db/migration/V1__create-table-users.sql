3f0fd22f7f193d8b68d7ed66f87b1db24c13ad43
Isaac Andrade <isaac@pop-os.localdomain>
2024-05-01 03:46:15 -0300

    :sparkles: feat: Adding Flyway migration

---
+++ API_Rest/src/main/resources/db/migration/V1__create-table-users.sql
@@ -0,0 +1,6 @@
+CREATE TABLE public.users (
+    id bigint NOT NULL,
+    user_name character varying(50) NOT NULL,
+    address character varying(100) NOT NULL,
+    gender character varying(6) NOT NULL
+);
---
+++ API_Rest/src/main/resources/db/migration/V3__alter-table-users.sql
@@ -0,0 +1,2 @@
+CREATE SEQUENCE users_id_seq;
+ALTER TABLE users ALTER COLUMN id SET DEFAULT nextval('users_id_seq');
